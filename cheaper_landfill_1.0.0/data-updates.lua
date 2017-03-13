1b51098b08ab84d2c30f4a4c7760c6ba15941726
MarcGamesons <MarcGamesons@users.noreply.github.com>
2017-03-13 09:46:49 +0100

    Added the project to Github
    The sourcecode of the "Cheaper Landfill" mod made by MarcGamesons can
    now be found on github.com.

---
+++ cheaper_landfill_1.0.0/data-updates.lua
@@ -0,0 +1,17 @@
+data:extend({
+
+{
+   type = "recipe",
+   name = "landfill",
+   energy_required = 0.5,
+   enabled = true,
+   category = "crafting",
+   ingredients =
+   {
+     {"stone", 20}
+   },
+   result= "landfill",
+   result_count = 20
+ }
+
+})
