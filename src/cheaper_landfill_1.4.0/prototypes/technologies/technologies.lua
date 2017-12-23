58d405e545895d989939f8d5d6a1738b8588f16c
MarcGamesons <MarcGamesons@users.noreply.github.com>
2017-12-23 01:26:30 +0100

    Updated License Information
    Updated all files with the new License information.

--- src/cheaper_landfill_1.4.0/prototypes/technologies/technologies.lua
+++ src/cheaper_landfill_1.4.0/prototypes/technologies/technologies.lua
@@ -1,6 +1,6 @@
 --[===[
-    This file is part of https://github.com/MarcGamesons/factoriomod-cheaper-landfill which is released under UNLICENSE.
-    Go to https://github.com/MarcGamesons/factoriomod-cheaper-landfill/blob/master/UNLICENSE for full license details.
+    This file is part of https://github.com/MarcGamesons/factoriomod-cheaper-landfill which is released under MIT License.
+    Go to https://opensource.org/licenses/MIT for full license details.
     ===
     For support open a new issue at https://github.com/MarcGamesons/factoriomod-cheaper-landfill/issues/new or visit https://forums.factorio.com/viewtopic.php?f=93&t=44991.
 ]===]
